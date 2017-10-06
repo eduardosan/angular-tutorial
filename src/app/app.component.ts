de9ca525056f69013e787cc9e5a531fe00f0b5df
aluno <aluno@localhost.localdomain>
2017-10-06 10:52:27 -0300

    Arquivos do tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+    import { Component } from '@angular/core';
+     
+    @Component({
+      selector: 'app-root',
+      template: `
+        <h1>{{title}}</h1>
+        <a routerLink="/dashboard">Dashboard</a>
+	<a routerLink="/heroes">Heroes</a>
+	<router-outlet></router-outlet>
+      `
+    })
+    export class AppComponent {
+      title = 'Tour of Heroes';
+    }
